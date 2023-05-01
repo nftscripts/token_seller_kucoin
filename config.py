1e1f0e59d7f0839fc2eeffdbaf0267004bd3bed6
nftscripts <121122262+nftscripts@users.noreply.github.com>
2023-05-01 22:42:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ COIN = 'SUI'
 
 MIN_PRICE: float = 0
 list_time = datetime(year=2022, month=5, day=3, hour=15, minute=0, second=0)
-COEFFICIENT = 1
+
 """ time.is - website to watch time
 If they lag behind - (how far behind) 
 If they're in a hurry (how much hurry) """
